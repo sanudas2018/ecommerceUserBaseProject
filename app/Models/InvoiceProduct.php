2f35dcd8539b56a12cef549b240ef92145de21b2
sanudas2018 <sanu.das08103@gmail.com>
2024-09-14 01:01:26 +0600

    Model and Migration Done all project

---
+++ app/Models/InvoiceProduct.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class InvoiceProduct extends Model
+{
+    protected $fillable = ['invoice_id', 'product_id', 'qty', 'sale_price','user_id'];
+
+    public function product(): BelongsTo
+    {
+        return $this->belongsTo(Product::class);
+    }
+}
